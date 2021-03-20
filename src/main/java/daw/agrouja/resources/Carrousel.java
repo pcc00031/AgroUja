a118c31ce5ecc04b8623baedc1d31ebc2257ec97
pcc00031 <pcc00031@red.ujaen.es>
2021-03-20 12:39:18 +0100

    Modificación ficheros

--- src/main/java/daw/agrouja/resources/Carrousel.java
+++ src/main/java/daw/agrouja/resources/Carrousel.java
@@ -1,34 +1,30 @@
 package daw.agrouja.resources;
 
-
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-import javax.faces.bean.ManagedBean;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Equipo
  */
-    
-@Named("carr")
-@RequestScoped
-public class Carrousel {
+@Named(value = "carr")
+public class Carrousel implements Serializable {
 
     private List<String> images;
 
     @PostConstruct
     public void init() {
+
         images = new ArrayList<String>();
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i < 3; i++) {
             images.add("img" + i + ".jpg");
         }
     }
@@ -36,5 +32,5 @@ public class Carrousel {
     public List<String> getImages() {
         return images;
     }
-}
 
+}
