5b1f064c0c6c8ff7e1dedfdbfa58bfa31bd00079
pcc00031 <pcc00031@red.ujaen.es>
2021-05-02 10:13:56 +0200

    Arreglos merge - Final

--- src/main/java/daw/agrouja/controller/ContactoController.java
+++ src/main/java/daw/agrouja/controller/ContactoController.java
@@ -23,103 +23,104 @@ import javax.inject.Named;
  *
  * @author Carlos
  */
-
 @Named("ctrlFormulario")
 @ViewScoped
 public class ContactoController implements Serializable {
 
     private static final Logger logger = Logger.getLogger(ContactoController.class.getName());
 
-    
-    private boolean bar=false;
+    private boolean bar = false;
     private int contador = 1;
-    
-    @Inject @DAOJpa
+
+    @Inject
+    @DAOJpa
     private FormulariosDAO formulariosDAO;
-    
+
     private Formulario formulario;
-    
+
     List<Formulario> formularios;
-    
-    @Inject @DAOJpa
+
+    @Inject
+    @DAOJpa
     private UsuarioDAO usu;
-    
+
     @Inject
     private Principal principal;
-    
-    public ContactoController(){   
+
+    public ContactoController() {
     }
-    
+
     @PostConstruct
-    private void init (){
-        formulario=new Formulario();
-        formularios=formulariosDAO.buscaTodos();
+    private void init() {
+        formulario = new Formulario();
+        formularios = formulariosDAO.buscaTodos();
     }
-    
-    public Formulario getFormulario(){
+
+    public Formulario getFormulario() {
         return formulario;
     }
-    
-    public void setFormulario(Formulario formulario){
-        this.formulario=formulario;
+
+    public void setFormulario(Formulario formulario) {
+        this.formulario = formulario;
     }
-    
-    public List<Formulario> getFormularios(){
+
+    public List<Formulario> getFormularios() {
         logger.info("Buscando formularios");
         return formularios;
     }
-    
-    public String contForm(){
-        return "Formulario"+contador++;
+
+    public String contForm() {
+        return "Formulario" + contador++;
     }
-    
-    public String crea(){
+
+    public String crea() {
         logger.info("Guardando formulario");
         formulario.setIdUsuario(usu.buscaPorNombre(principal.getName()).getId());
         formulariosDAO.crea(formulario);
-        
+
         //formularios=formulariosDAO.buscaTodos();
         return "/contacto/detalle?faces-redirect=true&id=" + formulario.getId();
     }
-    
+
     public String borrar(Integer id) {
         logger.info("Borrando formulario");
         formulariosDAO.borra(id);
         return "/usuario/mostrar?faces-redirect=true";
     }
 
-    public void recupera(){
+    public void recupera() {
         logger.log(Level.INFO, "Recuperando formulario {0}", formulario.getId());
-        formulario=formulariosDAO.buscaId(formulario.getId());
+        formulario = formulariosDAO.buscaId(formulario.getId());
 
     }
-    
-    public void recupera(Integer id){
+
+    public void recupera(Integer id) {
         logger.log(Level.INFO, "Recuperando formulario {0}", id);
         formulario = formulariosDAO.buscaId(id);
     }
-    
-    public int isContador(){
+
+    public int isContador() {
         return contador;
     }
-    
-    public void foo(){
-        bar=!bar;
+
+    public void foo() {
+        bar = !bar;
     }
-    
-    public boolean isBar(){
+
+    public boolean isBar() {
         return bar;
     }
-    
-    public void setBar(boolean bar){
-        this.bar=bar;
-    }       
-    
-    public void UsuarioCont(Integer usu){
-       formulario.setIdUsuario(usu);
+
+    public void setBar(boolean bar) {
+        this.bar = bar;
     }
-    
+
+    public void UsuarioCont(Integer usu) {
+        formulario.setIdUsuario(usu);
+    }
+
     public String visualiza(Integer Id) {
+        System.out.println(Id);
         formulario = formulariosDAO.buscaId(Id);
         return "/contacto/detalle?faces-redirect=true&id=" + formulario.getId();
     }
